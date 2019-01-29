f6f083a00d93bc1fbc35a93602ae1c7306b2dc36
yo <dims.ltw@gmail.com>
2019-01-29 14:54:13 +0200

    Second version

--- indus.php
+++ indus.php
@@ -1,49 +1,26 @@
 <?php
 $array = [-2,1,2,-3,-2,-4,5,7,-1,45,61,33,-1,-5,-6,4,4,4,-66];
-$new_middle_plus = [];
-$new_middle_minus = [];
 $res_minus = [];
 $res_plus = [];
-$start=1;
 
-switch ($start++){
+for ($i=0;$i<count($array);$i++){
+    $first = $array[$i];
+    $second = $array[$i+1];
+    $third = $array[$i+2];
 
-    case 1:
-        for ($i=0;$i<count($array);$i++){
-            if ($array[$i]<0) {
-                $b++;
-                array_push($new_middle_minus, $array[$i]);
-                if ($b == 3){
-                    $res_minus = array_merge($res_minus,$new_middle_minus);
-                    $value_minus = count($res_minus)/3;
-                }
-            }
-            elseif ($array[$i]>0){
-                $b = 0;
-                $new_middle_minus = [];
-            }
+    if ($first<0 && $second<0 && $third<0) {
+        for ($k = $i; $k <= $i + 2; $k++) {
+            $res_minus[] = $array[$k];
         }
-        print_r($res_minus);
-        echo "</br>";
-        echo 'Количество отрицательных пар:'.$value_minus."</br>";
+    }
 
-    case 2:
-        for ($j=0;$j<count($array);$j++){
-            if ($array[$j]>0) {
-                $g++;
-                array_push($new_middle_plus, $array[$j]);
-                if ($g == 3){
-                    $res_plus = array_merge($res_plus,$new_middle_plus);
-                    $value_plus = count($res_plus)/3;
-                }
-            }
-            elseif ($array[$j]<0){
-                $g = 0;
-                $new_middle_plus = [];
-            }
+    if ($first>0 && $second>0 && $third>0){
+        for ($h = $i; $h <= $i + 2; $h++) {
+            $res_plus[] = $array[$h];
         }
-        print_r($res_plus);
-        echo "</br>";
-        echo 'Количество положительных пар:'.$value_plus."</br>";
-        break;
+    }
 }
+echo "</br>";
+print_r($res_minus);
+echo "</br>";
+print_r($res_plus);
